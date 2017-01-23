9cec2245555d84bf7dcb79390290366892bfa73d
Yugender Boini <yboini@yahoo-inc.com>
2017-01-23 11:49:24 -0800

    Initial Commit

---
+++ flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flicks
+//
+//  Created by  Yugender Boini on 1/23/17.
+//  Copyright © 2017 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
